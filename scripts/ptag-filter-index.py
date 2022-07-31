9017eb859f1d59ca2f87ce76dccdd17781b95610
thautwarm <twshere@outlook.com>
2022-08-01 01:03:01 +0800

    fix scoping issues of ptag-filter-index & fix #1

--- scripts/ptag-filter-index.py
+++ scripts/ptag-filter-index.py
@@ -10,9 +10,10 @@ import bs4
 
 
 class FilterIndex:
-    def __init__(self, pattern: P, html_id: str):
+    def __init__(self, scope: dict, pattern: P, html_id: str):
         self.P = pattern
         self.html_id = html_id
+        self.scope = scope
 
     def __call__(self, doc: OPDocument):
         html = bs4.BeautifulSoup(doc.code, "html.parser")
@@ -23,7 +24,7 @@ class FilterIndex:
         for each in doc.project_docs.values():
             each.working_dir_absolute
 
-            if match_any_tag(self.P, each.tags):
+            if match_any_tag(self.P, each.tags, self.scope):
                 path = str(pathlib.Path(each.project_based_path).with_suffix(".html"))
                 title = each.title or each.output_path_absolute.name
                 li = html.new_tag("li")
@@ -46,19 +47,21 @@ class PTagQueryDocsEntry(CommandEntry):
 
     def proc(self, argv: list[str], start: int, end: int):
         ctx = self.ctx
+        scope = self.ctx.storage.setdefault(names.NAME_PythonScope, {})
         source = process_nest(self.ctx, start, end)
         patterns = string_pattern_builder(source)
         if not patterns:
             raise ValueError("At least one pattern is required.")
         matcher = functools.reduce(cps_and, patterns)
         html_id = global_gensym("filtered-index")
-        ctx.target_doc.callbacks.append(FilterIndex(matcher, html_id))
+        ctx.target_doc.callbacks.append(FilterIndex(scope, matcher, html_id))
         return f'<ul id="{html_id}"></ul>'
 
     def inline_proc(self, _start: int, _stop: int) -> str:
         ctx = self.ctx
+        scope = self.ctx.storage.setdefault(names.NAME_PythonScope, {})
         source = process_nest(self.ctx, _start, _stop)
         pattern = string_to_pattern(source)
         html_id = global_gensym("filtered-index")
-        ctx.target_doc.callbacks.append(FilterIndex(pattern, html_id))
+        ctx.target_doc.callbacks.append(FilterIndex(scope, pattern, html_id))
         return f'<ul id="{html_id}"></ul>'
